c60ccda2bde856995449d9e43246cbc5b27e3aca
night-runner-dh <161902344+night-runner-dh@users.noreply.github.com>
2024-04-01 07:19:25 +0000

    フォロー機能 ok

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,11 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
    end
   
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+  	get "followings" => "relationships#followings", as: "followings"
+  	get "followers" => "relationships#followers", as: "followers"
+  end
   
   resources :books do
     resource :favorite, only: [:create, :destroy]
