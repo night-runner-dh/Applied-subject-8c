de533f203bac918f7c496eeabf940f7570f0015d
night-runner-dh <161902344+night-runner-dh@users.noreply.github.com>
2024-04-09 11:27:46 +0000

    グループ作成 仮3

--- db/migrate/20240406101753_create_group_users.rb
+++ db/migrate/20240406101753_create_group_users.rb
@@ -3,7 +3,7 @@ class CreateGroupUsers < ActiveRecord::Migration[6.1]
     create_table :group_users do |t|
     
      t.references :user, foreign_key: true
-    t.references :group, foreign_key: true
+     t.references :group, foreign_key: true
       t.timestamps
     end
   end
